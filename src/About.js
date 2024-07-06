702c26ca901b700ca46cbaa2064a894c469d3b55
Niviss <118551653+Niviss@users.noreply.github.com>
2024-07-06 18:22:02 +0530

    Add files via upload

---
+++ src/About.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import "./About.css"
+import images from "./images/images.jpg"
+
+const About = ({data}) => {
+/*
+Here
+data=1 means true
+data=2 means false
+*/ 
+  return (
+      <div data-theme={data===1?"Dark":"Light"}className='About-page'>
+        <div className='cont1-about'>
+            <div className='prof-back'>
+                <div className='img1'>
+                  <div className='img2'>
+                  <img src={images}/>
+                  </div>
+                </div>
+                <div className='img-name'>
+            <span><b>NIVISS.K</b></span><br/>
+            <span>Web Developer</span>
+                </div>
+              </div>
+            <div className='Skills'>
+            <h1>My Skills</h1>
+            <li><h4>Java</h4>
+            <span className='bar'><span className='java'></span></span>
+            </li>
+            <li><h4>HTML</h4>
+            <span className='bar'><span className='html'></span></span>
+            </li>
+            <li><h4>CSS</h4>
+            <span className='bar'><span className='css'></span></span>
+            </li>
+            <li><h4>Javascript</h4>
+            <span className='bar'><span className='javascript'></span></span>
+            </li>
+            <li><h4>React</h4>
+            <span className='bar'><span className='react'></span></span>
+            </li>
+          </div>
+        </div>
+        <div className='cont2-about'>
+          <div className='details'>
+          <h4>My Intro</h4>
+          <h1>About Me</h1>
+          <p>
+            <b>Niviss</b> loves programming, sketching, traveling and playing cricket.
+            As a recent graduate with a passion for web development and a strong foundation
+            in front-end-development. Throughout my academic journey, I have gained valuable experience 
+            in HTML, CSS, Javascript and Java,which allowed me to develop responsive and visually appealing websites.
+            In addition to my technical skills, I am a quick learner and have a strong desire to continuously improve my abilities.
+            I am confident that my passion for front-end development, coupled with my dedication to staying up-to-date with the industry trends.
+          </p>
+          <table>
+            <tr>
+              <th>Name   : </th>
+              <td>Niviss.K</td>
+            </tr>
+            <tr>
+              <th>Degree   : </th>
+              <td>B.E-EEE</td>
+            </tr>
+            <tr>
+              <th>D.O.B   : </th>
+              <td>6 Feb 2002</td>
+            </tr>
+            <tr>
+              <th>Address   : </th>
+              <td>Vellakovil,Tamil Nadu</td>
+            </tr>
+            <tr>
+              <th>Country   : </th>
+              <td>India</td>
+            </tr>
+            <tr>
+              <th>Email   : </th>
+              <td>kuppusamy4082@gmail.com</td>
+            </tr>
+          </table>
+          </div>
+        </div>
+      </div>
+
+  )
+}
+
+export default About
---
+++ src/Contact.js
@@ -0,0 +1,153 @@
+import React from 'react'
+import "./Contact.css";
+import Marquee from "react-fast-marquee"
+import star from "./images/star.png"
+import { useState} from 'react';
+import instagram from "./images/instagram.png"
+import github from "./images/github.png"
+import linkedin from "./images/linkedin.png"
+import emailjs from "@emailjs/browser"
+
+const Contact = ({data}) => {
+  const [name,setname]=useState('');
+  const [email,setemail]=useState('');
+  const [subj,setsubj]=useState("");
+  const [msg,setmsg]=useState('')
+  
+
+  const handleSubmit=async(e)=>{
+    e.preventDefault();
+
+    const serviceid="service_we3zfvy";
+    const templateid="template_krqjv2m";
+    const publicKey="fE2z_eG9BsbSSyzqh";
+  
+
+     const template_params={
+        name:name,
+        email:email,
+        subj:subj,
+        msg:msg,
+      };
+
+      emailjs.send(serviceid,templateid,template_params,publicKey)
+      .then((response)=>{
+        alert("email sent successfully");
+        console.log("email sent successfully");
+        setname("") 
+        setemail("")
+        setsubj("")
+        setmsg("")
+      }).catch((error)=>{
+        alert("error in sending email")
+        console.log("error sending mail")
+    })
+
+   
+  }
+  return (
+    <div data-theme={data===1?"Dark":"Light"} className="Contact-page">
+    <div className='container-CP'>
+        <div className='laptop'>
+          <div className='content'>
+            <div className='marquee'>
+            <Marquee>
+            <div  className='marquee-list'>
+            <div className='marquee-cont'>
+              <h3>Contact Me</h3>
+              <div className='marq-img'>
+                <img src={star}/>
+              </div>
+            </div>
+
+            <div className='marquee-cont'>
+              <h3>Contact Me</h3>
+              <div className='marq-img'>
+                <img src={star}/>
+              </div>
+            </div>
+
+            <div className='marquee-cont'>
+              <h3>Contact Me</h3>
+              <div className='marq-img'>
+                <img src={star}/>
+              </div>
+            </div>
+
+            <div className='marquee-cont'>
+              <h3>Contact Me</h3>
+              <div className='marq-img'>
+                <img src={star}/>
+              </div>
+            </div>
+
+            </div>
+            </Marquee>
+            </div>
+            <div className='contact-info'>
+              <h6>I'LL BE GLAD TO ANSWER YOUR QUESTION</h6>
+              <h6>--XXX--</h6>
+                <form onSubmit={handleSubmit}>
+                  <input type='text'
+                  placeholder='Name'
+                  className='form-data'
+                  value={name}
+                  onChange={(e)=>setname(e.target.value)}
+                  required
+                  />
+                  <input type="email"
+                   placeholder='Email address'
+                   className='form-data'
+                   value={email}
+                   onChange={(e)=>setemail(e.target.value)}
+                   required
+                   />
+                  <input type='text' 
+                  placeholder='Subject'
+                  className='form-data'
+                  value={subj}
+                  onChange={(e)=>setsubj(e.target.value)}
+                  required
+                  />
+                  <textarea name="postContent"
+                   placeholder='your messge'
+                   className='form-datamsg'
+                   value={msg}
+                   onChange={(e)=>setmsg(e.target.value)}
+                   required
+                   />
+                    <div className='verification'>
+                   <button className='buttonsub'>Send Message </button>
+                    </div>             
+                </form>
+            </div>
+            
+          </div>
+        </div>
+    <footer>
+      <div className='footer'>
+        <ol className='prof-icons-CP'>
+            <li><a href="https://www.instagram.com/invites/contact/?i=1iuqr392gvnh9&utm_content=butp6ln" rel="noreferrer"target="_blank" alt="Instagram">
+              <img src={instagram} alt='Instagram'/></a></li>
+            <li><a href="https://github.com/Niviss" alt="github" target='_blank'rel="noreferrer"><img src={github} alt="Github"/></a></li>
+            <li><a href="https://www.linkedin.com/in/niviss-k-51659b219" rel="noreferrer"target='_blank' alt="linkedin">
+              <img src={linkedin} alt='LinkedIn'/></a></li>
+      </ol>
+        <ul className='nav-links-CP'>
+            <li><a href="#Home-Page">Home</a></li>
+            <li><a href="#About-Page">About</a></li>
+            <li><a href="c">Project</a></li>
+            <li><a href="#Contact-Page">Contact</a></li>
+        </ul>
+      </div>
+      <div className='copyr'>
+        <p>Copyright &copy;2024; Designed by <span>NIVISS</span></p>
+      </div>
+    </footer>
+  </div>
+    </div>
+  )
+}
+
+
+export default Contact
